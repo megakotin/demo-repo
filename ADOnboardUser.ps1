573a64243208cc999ad9dcfaffda0d3ee6c6a658
megakotin <32658773+megakotin@users.noreply.github.com>
2026-07-19 11:21:17 +0800

    Create ADOnboardUser.ps1

---
+++ ADOnboardUser.ps1
@@ -0,0 +1,12 @@
+<#
+Issue: #2 - AD onboard user
+
+Features:
+- Create AD user
+- Assign Manager
+- Move to Department OU
+- Add Security Groups
+- Set mailNickname
+- Set msExchHideFromAddressLists
+#>
+
